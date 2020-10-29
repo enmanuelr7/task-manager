601c4d301f4b631fe579351f4dd5a85aa04cddbf
Enmanuel Dario Ramirez Villamizar <e.ramirez.villamizar@accenture.com>
2020-10-28 21:55:13 -0300

    add timestamps to user and task model

--- src/models/task.js
+++ src/models/task.js
@@ -15,7 +15,9 @@ const taskSchema = new mongoose.Schema({
         required: true,
         ref: 'User'
     }
-});
+}, {
+    timestamps: true
+})
 
 const Task = mongoose.model('Task', taskSchema)
 
