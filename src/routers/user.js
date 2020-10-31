e0109457c9f567b4d7474dfcaaa381e9d6ea86ee
Enmanuel Dario Ramirez Villamizar <e.ramirez.villamizar@accenture.com>
2020-10-31 10:50:39 -0300

    serving up files

--- src/routers/user.js
+++ src/routers/user.js
@@ -132,4 +132,20 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     res.send()
 })
 
+// get profile picture
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    } catch (error) {
+        res.status(404).send()
+    }
+})
+
 module.exports = router
