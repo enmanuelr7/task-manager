91c2313310006a1425f61fe5b7cef7d5de604b5b
Enmanuel Dario Ramirez Villamizar <e.ramirez.villamizar@accenture.com>
2020-11-02 21:02:26 -0300

    env jwt

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
+require('dotenv').config()
 
 const auth = async (req, res, next) => {
 
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'secretstring')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
         if (!user) {
